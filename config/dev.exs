6b388e025f8595ae2f3066ee0efe38551edc52c2
Heather You <heather.fswd@gmail.com>
2019-03-14 14:31:20 -0400

    Fix test

--- config/dev.exs
+++ config/dev.exs
@@ -11,23 +11,10 @@ config :crud_app, CrudAppWeb.Endpoint,
   debug_errors: true,
   code_reloader: true,
   check_origin: false,
-  # watchers: [
-  #   yarn: [
-  #     "run", 
-  #     "watch", 
-  #     cd: Path.expand("../assets", __DIR__)
-  #   ]
-  # ]
   watchers: [
-    # node: [
-    #   "node_modules/webpack/bin/webpack.js",
-    #   "--mode",
-    #   "development",
-    #   "--watch-stdin",
-    #   cd: Path.expand("../assets", __DIR__)
-    # ],
     npm: [
-      "run", "serve", 
+      "run",
+      "serve", 
       cd: Path.expand("../assets", __DIR__)
     ]
   ]
