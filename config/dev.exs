255d219b501db0cffd307aaa62c2e61a2ced3638
Heather You <heather.fswd@gmail.com>
2019-03-15 10:47:33 -0400

    Set up Vue.js with Jest

--- config/dev.exs
+++ config/dev.exs
@@ -11,20 +11,11 @@ config :crud_app, CrudAppWeb.Endpoint,
   debug_errors: true,
   code_reloader: true,
   check_origin: false,
-  # watchers: [
-  #   yarn: [
-  #     "run", 
-  #     "watch", 
-  #     cd: Path.expand("../assets", __DIR__)
-  #   ]
-  # ]
   watchers: [
-    node: [
-      "node_modules/webpack/bin/webpack.js",
-      "--mode",
-      "development",
-      "--watch-stdin",
-      cd: Path.expand("../assets", __DIR__)
+    npm: [
+      "run",
+      "serve",
+      cd: Path.expand("../assets", __DIR__)]
     ]
   ]
 
