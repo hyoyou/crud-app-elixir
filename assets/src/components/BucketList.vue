7976b7bc080a8bc9889d227a8c99bea0cbd74887
Heather You <heather.fswd@gmail.com>
2019-03-25 17:05:00 -0400

    Rerender index page when form is submitted

--- assets/src/components/BucketList.vue
+++ assets/src/components/BucketList.vue
@@ -1,17 +1,23 @@
 <template>
   <div class="bucket-list">
+    <div class="error" v-if="error">
+        {{ error }}
+    </div>
+
     <h1>{{ msg }}</h1>
     <h3>Current Goals</h3>
-      <ul id="current-goals" v-for="(goal, index) in goals" :key="index">
+      <div class="list-of-goals">
+        <ul id="current-goals" v-for="goal in goals" :key="goal.id">
           <li>I want to {{ goal.activity }} in {{ goal.location }}.</li>
-      </ul>
+        </ul>
+      </div>
     <h3>Add to the Bucket!</h3>
       <form>
         <div id="new-goal">
           I want to <input type="text" name="activity" v-model="activity" placeholder="insert activity here!"> 
           in <input type="text" name="location" v-model="location" placeholder="insert location here!">.
 
-          <button type="submit" name="submit" v-on:click="postGoal">Add</button>
+          <button type="submit" name="submit" v-on:click.prevent="postGoal">Add</button>
         </div>
       </form>
   </div>
@@ -30,27 +36,35 @@ export default {
       goals: [],
       activity: '',
       location: '',
-      errors: []
+      error: null
     }
+  },
+  created () {
+    this.fetchGoals()
   },
   methods: {
     postGoal: function () {
+      this.newGoal = { activity: this.activity, location: this.location }
       axios.post(`http://localhost:4000/api/goals`,
-        { goal: { activity: this.activity, location: this.location } },
+        { goal: this.newGoal },
         { headers: {
           'Content-type': 'application/json',
         }}
       )
+      this.goals.push(this.newGoal)
+      this.activity = this.location = ''
+    },
+    fetchGoals: function () {
+      this.error = this.goals = null
+
+      axios.get(`http://localhost:4000/api/goals`)
+      .then(response => {
+        this.goals = response.data.data
+      })
+      .catch(error => {
+        this.error = error
+      })
     }
-  },
-  mounted () {
-    axios.get(`http://localhost:4000/api/goals`)
-    .then(response => {
-      this.goals = response.data.data
-    })
-    .catch(error => {
-      this.errors.push(error)
-    })
   }
 }
 </script>
