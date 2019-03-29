7069aca6cb354d7bde57c367025fcec1c4b224fa
Heather You <heather.fswd@gmail.com>
2019-03-29 16:28:01 -0400

    Added constants, but it is only accessible within module

--- lib/crud_app_web/views/error_view.ex
+++ lib/crud_app_web/views/error_view.ex
@@ -1,21 +1,15 @@
 defmodule CrudAppWeb.ErrorView do
   use CrudAppWeb, :view
+  
+  @not_found %{detail: "Endpoint Not Found"}
+  @server_error %{detail: "Internal Server Error"}
 
-  # If you want to customize a particular status code
-  # for a certain format, you may uncomment below.
-  # def render("500.html", _assigns) do
-  #   "Internal Server Error"
-  # end
-
-  # By default, Phoenix returns the status message from
-  # the template name. For example, "404.html" becomes
-  # "Not Found".
   def render("404.json", _assigns) do
-    %{errors: %{detail: "Endpoint Not Found"}}
+    %{errors: @not_found}
   end
 
   def render("500.json", _assigns) do
-    %{errors: %{detail: "Internal Server Error"}}
+    %{errors: @server_error}
   end
 
   def template_not_found(template, _assigns) do
