c5dc4fc28d415c2ca087bef6b0b801f0431f41ce
Heather You <heather.fswd@gmail.com>
2019-04-16 10:04:10 -0400

    Try to switch to AWS environment variables

--- rel/config/config.exs
+++ rel/config/config.exs
@@ -2,16 +2,16 @@ use Mix.Config
 
 config :crud_app, CrudApp.Repo,
   adapter: Ecto.Adapters.Postgres,
-  database: System.get_env("POSTGRES_DB"),
-  username: System.get_env("POSTGRES_USER"),
-  password: System.get_env("POSTGRES_PASS"),
-  hostname: System.get_env("POSTGRES_HOST"),
-  port: System.get_env("PORT"),
+  database: System.get_env("RDS_DB_NAME"),
+  username: System.get_env("RDS_USERNAME"),
+  password: System.get_env("RDS_PASSWORD"),
+  hostname: System.get_env("RDS_HOSTNAME"),
+  port: System.get_env("RDS_PORT"),
   pool_size: 15
 
 config :crud_app, CrudApp.Endpoint,
   http: [:inet6, port: 4000],
-  url: [scheme: "http", host: System.get_env("HOST"), port: 80],
+  url: [scheme: "http", host: System.get_env("RDS_PORT"), port: 80],
   server: true,
   cache_static_manifest: "priv/static/cache_manifest.json",
   secret_key_base: System.get_env("SECRET_KEY_BASE")
