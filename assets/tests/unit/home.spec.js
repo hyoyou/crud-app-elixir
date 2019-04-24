61d994d78448cd8e04d37d145f33afd66577af90
Heather You <heather.fswd@gmail.com>
2019-04-24 14:45:33 -0400

    Add button to mark as acheived

--- assets/tests/unit/home.spec.js
+++ assets/tests/unit/home.spec.js
@@ -43,13 +43,13 @@ describe('BucketList.vue', () => {
       ]
     }
 
-    return expect(wrapper.vm.fetchGoals()).resolves.toEqual(expected);
+    expect(wrapper.vm.fetchGoals()).resolves.toEqual(expected);
   })
 
-  it('makes a POST request when the button is clicked', () => {
+  it('calls the postGoal function when the "submit" button is clicked', () => {
     const spy = jest.spyOn(BucketList.methods, 'postGoal')
     const wrapper = shallowMount(BucketList)
-    wrapper.find('button').trigger('click')
+    wrapper.find('#submit-btn').trigger('click')
 
     expect(spy).toHaveBeenCalled();
   })
@@ -68,20 +68,54 @@ describe('BucketList.vue', () => {
     expect(wrapper.vm.location).toEqual('awesome place')
   })
 
-  it('does not make a POST request when the button isn\'t clicked', () => {
+  it('does not call the postGoal function when the button isn\'t clicked', () => {
     const spy = jest.spyOn(BucketList.methods, 'postGoal')
     shallowMount(BucketList)
 
     expect(spy).not.toHaveBeenCalled()
   })
 
-  it('makes a call to the post action of the API when user fills out the form and the button is clicked', done => {
+  it('makes a call to the create action of the API when user fills out the form and the button is clicked', done => {
     const wrapper = shallowMount(BucketList)
     wrapper.find('[data-activity]').setValue('sample activity')
     wrapper.find('[data-location]').setValue('sample location')
-    wrapper.find('button').trigger('click')
+    wrapper.find('#submit-btn').trigger('click')
+
     wrapper.vm.$nextTick(() => {
       expect(wrapper.vm.goals).toEqual([{ activity: 'sample activity', location: 'sample location'}])
+      done()
+    })
+  })
+
+  it('calls the updateGoal function when button to mark "achieved" is clicked', async () => {
+    const spy = jest.spyOn(BucketList.methods, 'updateGoal')
+    const wrapper = shallowMount(BucketList)
+
+    await wrapper.vm.fetchGoals()
+    wrapper.find('#achieved-btn').trigger('click')
+
+    expect(spy).toHaveBeenCalled();
+  })
+
+  it('makes a call to the update action in the API when button to mark "achieved" is clicked', async (done) => {
+    const wrapper = shallowMount(BucketList)
+
+    await wrapper.vm.fetchGoals()
+    wrapper.find('#achieved-btn').trigger('click')
+
+    wrapper.vm.$nextTick(() => {
+      expect(wrapper.vm.goals).toEqual({
+        "goals": [
+          { "activity": "swim with sharks", 
+            "id": 1, 
+            "is_achieved": true, 
+            "location": "the Bahamas" }, 
+          { "activity": "feed flamingos", 
+            "id": 2, 
+            "is_achieved": false, 
+            "location": "Aruba" }
+        ]
+      })
       done()
     })
   })
